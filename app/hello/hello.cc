d1ab0cbc4c6a92289a7708d91408ede0adcb5c97
Flash-LHR <47357603+Flash-LHR@users.noreply.github.com>
2025-06-03 15:08:52 +0800

    cov 11 (#2)
    * cov 11
    
    * cov 12
    
    * cov 13
    
    * cov 14
    
    * cov 15
    
    * cov 16
    
    * cov 17
    
    * cov 18
    
    * cov 19
    
    * cov 20
    
    * cov 21

--- app/hello/hello.cc
+++ app/hello/hello.cc
@@ -4,11 +4,27 @@
 #include <string>
 
 std::string hello(int x) noexcept {
-  if (x == 0) {
+    if (x == 0) {
     int y = x + 1;
     int z = x * y;
     std::cout << x << std::endl;
     std::cout << y << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
+    std::cout << z << std::endl;
     std::cout << z << std::endl;
     std::cout << z << std::endl;
     std::cout << z << std::endl;
